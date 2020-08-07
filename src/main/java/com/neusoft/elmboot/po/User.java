e4c7be3c5f3103b37969c624554ca18af7b1a42d
lyyrhf <448191580@qq.com>
2020-08-07 15:51:33 +0800

    冉海锋--user

--- src/main/java/com/neusoft/elmboot/po/User.java
+++ src/main/java/com/neusoft/elmboot/po/User.java
@@ -1,71 +1,24 @@
 package com.neusoft.elmboot.po;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
-    private String userId;
-    private String password;
-    private String userName;
-    private Integer userSex;
-    private String userImg;
-    private Integer delTag;
-
-    public User() {}
-
-    public User(String userId,String password,String
-            userName,Integer userSex,String userImg) {
-        this.userId = userId;
-        this.password = password;
-        this.userName = userName;
-        this.userSex = userSex;
-        this.userImg = userImg;
-        this.delTag = 1;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getUserSex() {
-        return userSex;
-    }
-
-    public void setUserSex(Integer userSex) {
-        this.userSex = userSex;
-    }
-
-    public String getUserImg() {
-        return userImg;
-    }
-
-    public void setUserImg(String userImg) {
-        this.userImg = userImg;
-    }
-
-    public Integer getDelTag() {
-        return delTag;
-    }
-
-    public void setDelTag(Integer delTag) {
-        this.delTag = delTag;
-    }
+    private int num;
+    private String empno;
+    private String ename;
+    private Date bitrh;
+    private String idnum;
+    private int deptno;
+    private int job;
+    private Date entime;
+    private Date wtime;
+    private String form;
+    private String resource;
+    private String tel;
+    private String email;
 }
